38a3ea1a075238597142d024340525376b68a65d
kiranrajput97 <32011873+kiranrajput97@users.noreply.github.com>
2020-02-08 10:45:56 +0530

    Add files via upload

---
+++ testfile.java
@@ -0,0 +1,8 @@
+impor static org.junit.jupiter.api.Assertion.assertEquals;
+import org.junit.jupiter.api.Test;
+public class EmployeeTest{
+ Employee e = new Emploee("Karan",50000,50);
+@Test
+Public Void test(){
+assertEquals(e.callNetPay(),4700);
+}}
